8a915be47526bd4c7c1546fe2ccdd1eb27ce77dd
Elbtalkessel <rtfsc@pm.me>
2025-01-20 09:05:06 +0100

    Mon 20 Jan 2025 09:05:06 CET

--- config/plug/ui/bufferline.nix
+++ config/plug/ui/bufferline.nix
@@ -1,101 +1,15 @@
 { config, lib, ... }:
-let
-  colors = import ../../colors/${config.theme}.nix { };
-in
 {
   plugins = {
     bufferline = {
       enable = true;
       settings = {
         options = {
-          separator_style = "thin"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+          # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+          separator_style = "thin";
         };
-        highlights = lib.mkIf config.colorschemes.base16.enable {
-          # Buffer line background
-          fill = {
-            fg = "none";
-            bg = colors.base01;
-          };
-          # Active tab background
-          background = {
-            fg = "none";
-            bg = colors.base01;
-          };
-          buffer = {
-            fg = "none";
-            bg = "none";
-          };
-          buffer_selected = {
-            fg = colors.base05;
-            bg = "none";
-            italic = false;
-          };
-          buffer_visible = {
-            fg = colors.base05;
-            bg = "none";
-          };
-          close_button = {
-            fg = "none";
-            bg = colors.base01;
-          };
-          close_button_visible = {
-            fg = colors.base03;
-            bg = "none";
-          };
-          close_button_selected = {
-            fg = colors.base08;
-            bg = "none";
-          };
-          indicator_selected = {
-            fg = colors.base0E;
-            bg = "none";
-          };
-          indicator_visible = {
-            fg = colors.base0E;
-            bg = "none";
-          };
-          separator = {
-            fg = colors.base00;
-            bg = colors.base01;
-          };
-          separator_selected = {
-            fg = colors.base00;
-            bg = colors.base00;
-          };
-          separator_visible = {
-            fg = colors.base00;
-            bg = colors.base00;
-          };
-          modified = {
-            fg = colors.base03;
-            bg = colors.base01;
-          };
-          modified_visible = {
-            fg = colors.base00;
-            bg = colors.base01;
-          };
-          modified_selected = {
-            fg = colors.base0B;
-            bg = colors.base00;
-          };
-          tab_close = {
-            fg = colors.base00;
-            bg = colors.base00;
-          };
-          duplicate = {
-            bg = "none";
-            italic = false;
-          };
-          duplicate_visible = {
-            bg = "none";
-            italic = false;
-          };
-          duplicate_selected = {
-            fg = "none";
-            bg = colors.base01;
-            italic = false;
-          };
-        };
+        # Defaults work just fine
+        highlights = lib.mkIf config.colorschemes.base16.enable { };
       };
     };
   };
